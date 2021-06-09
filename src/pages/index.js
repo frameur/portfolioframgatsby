399265092a60c3f0581975963a198659efb1f15d
frameur <francoisramos6121@gmail.com>
2021-06-09 04:20:31 +0200

    installation dependencies and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,42 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+
+export default function Home({data}) {
+
+console.log(data);
+
+  return (
+    <Layout>
+    <section className={styles.header}>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>Web developer from l'ouest de la France.</p>
+        <Link className={styles.btn} to="/projects" >
+         Mon Portfolio Projects</Link>
+      </div>
+      <Img fluid={data.file.childImageSharp.fluid} />
+     
+    </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner3.webp"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+
+
+`
