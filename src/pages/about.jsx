0caca91312a82f2014ee6ca6051dff4a223f4c2e
frameur <francoisramos6121@gmail.com>
2021-08-08 12:35:15 +0200

    changement images home

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/Layout"
-
-
+// import Video from "../components/video"
 
 export default function About() {
-    return (
+  return (
     <Layout>
-        <div>
-            <h1>About Page</h1>
-            <p><h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4> Excepturi ipsa saepe a magnam ea, vel sequi veritatis quia. Exercitationem deleniti cumque consequuntur. Assumenda culpa vero in atque, numquam provident asperiores eius nemo hic beatae ipsa repudiandae possimus? Consequatur, ut quis quos amet fugit commodi. Magnam quasi enim eveniet pariatur ad expedita, obcaecati consequatur rem odio natus temporibus nihil aut velit vero impedit harum, ullam molestias laudantium. Animi deleniti reprehenderit ipsa explicabo ex optio nesciunt eum dicta cupiditate non a fugiat quod enim omnis, eaque corporis error praesentium atque at autem necessitatibus iusto! Doloribus molestias sed eius. Quos harum quidem sed! Aliquam sapiente ratione assumenda doloremque voluptatum veritatis nisi repellendus numquam tempora itaque nostrum necessitatibus odio, saepe labore fuga architecto provident illo est adipisci doloribus quis ullam perspiciatis iure incidunt. Repellat in omnis nobis non hic beatae? Nemo fugiat eius eligendi, ut magni blanditiis, explicabo maiores facilis, necessitatibus impedit possimus error!</p>
-        </div>
-       
-       
+      <div>
+        <h2 style={{ textAlign: "center", margin: "20px" }}>About Page</h2>
+        <p>
+          <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>{" "}
+          Excepturi ipsa saepe a magnam ea, vel sequi veritatis quia.
+          Exercitationem deleniti cumque consequuntur. Assumenda culpa vero in
+          atque, numquam provident asperiores eius nemo hic beatae ipsa
+          repudiandae possimus? Consequatur, ut quis quos amet fugit commodi.
+          Magnam quasi enim eveniet pariatur ad expedita, obcaecati consequatur
+          rem odio natus temporibus nihil aut velit vero impedit harum, ullam
+          molestias laudantium. Animi deleniti reprehenderit ipsa explicabo ex
+          optio nesciunt eum dicta cupiditate non a fugiat quod enim omnis,
+          eaque corporis error praesentium atque at autem necessitatibus iusto!
+          Doloribus molestias sed eius. Quos harum quidem sed! Aliquam sapiente
+          ratione assumenda doloremque voluptatum veritatis nisi repellendus
+          numquam tempora itaque nostrum necessitatibus odio, saepe labore fuga
+          architecto provident illo est adipisci doloribus quis ullam
+          perspiciatis iure incidunt. Repellat in omnis nobis non hic beatae?
+          Nemo fugiat eius eligendi, ut magni blanditiis, explicabo maiores
+          facilis, necessitatibus impedit possimus error!
+        </p>
+      </div>
+
+      {/* <Video /> */}
     </Layout>
-    )
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,39 +4,34 @@ import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 import Img from "gatsby-image"
 
-
-
-export default function Home({data}) {
-
-console.log(data);
+export default function Home({ data }) {
+  console.log(data)
 
   return (
     <Layout>
-    <section className={styles.header}>
-      <div>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p>Web developer from l'ouest de la France.</p>
-        <Link className={styles.btn} to="/projects" >
-         Mon Portfolio Projects</Link>
-      </div>
-      <Img fluid={data.file.childImageSharp.fluid} />
-     
-    </section>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>Web developer from l'ouest de la France.</p>
+          <Link className={styles.btn} to="/projects">
+            Mon Portfolio
+          </Link>
+        </div>
+        <Img className={styles.img} fluid={data.file.childImageSharp.fluid} />
+      </section>
     </Layout>
   )
 }
 
 export const query = graphql`
-query Banner {
-  file(relativePath: {eq: "banner3.webp"}) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid
+  query Banner {
+    file(relativePath: { eq: "banner4.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
   }
-}
-
-
 `
